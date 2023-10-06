566a090b5d37ec68e28ae5062b1f2e3688d47184
Jaco Antonio <jaco42818@gmail.com>
2023-10-06 16:18:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import FileUploader from './components/FileUploader';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Tradutor de Arquivos</h1>
+      <FileUploader />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/fileUploader.jsx
@@ -0,0 +1,65 @@
+// FileUploader.js
+import { useState } from 'react';
+import axios from 'axios';
+
+const FileUploader = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', selectedFile);
+
+      const response = await axios.post('https://strong-beignet-130f7f.netlify.app/translate', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        responseType: 'arraybuffer'
+      });
+
+      console.log(response);
+
+        const blob = new Blob([response.data]);
+
+        // Cria um link temporário e simula um clique nele para iniciar o download
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'meu-pdf-traduzido.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+
+    } catch (error) {
+      console.error('Erro ao enviar o arquivo:', error);
+    }
+    
+  };
+
+//   useEffect(() => {
+//     const blob = new Blob([data]);
+
+//         // Cria um link temporário e simula um clique nele para iniciar o download
+//         const link = document.createElement('a');
+//         link.href = window.URL.createObjectURL(blob);
+//         link.download = 'arquivo.pdf';
+//         document.body.appendChild(link);
+//         link.click();
+//         document.body.removeChild(link);
+//   },[data])
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload} disabled={!selectedFile}>
+        Enviar Arquivo
+      </button>
+    </div>
+  );
+};
+
+export default FileUploader;
